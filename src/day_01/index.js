3d47413b18d554e2da33d880a206810fc7cee1ba
Joe Wright <joe@joejag.com>
2018-11-16 00:00:39 +0000

    refactor

--- src/day_01/index.js
+++ src/day_01/index.js
@@ -1,17 +1,27 @@
 import _ from 'lodash'
 
-const numberToArrayOfNumberParts = (number) => {
-  const numbersSplitAsText = number.toString().split('')
-  return numbersSplitAsText.map((string) => parseInt(string))
-}
+const numberToArrayOfNumberParts = (number) => number.toString().split('').map(Number)
 
-export const sum = (input) => {
+const sumOfMatchingPairs = (arrayOfPairs) => _.reduce(arrayOfPairs, (sum, [a, b]) => {
+  if (a !== b) return sum
+  return sum + a
+}, 0)
+
+export const sumOfWrappedPairs = (input) => {
   const numbers = numberToArrayOfNumberParts(input)
-  const wrappedNumbers = _.concat(_.tail(numbers), numbers[0])
+
+  const wrappedNumbers = _.concat(_.tail(numbers), _.head(numbers))
   const numbersZipped = _.zip(numbers, wrappedNumbers)
 
-  return _.reduce(numbersZipped, (sum, [a, b]) => {
-    if (a != b) return sum
-    return sum + a
-  }, 0)
+  return sumOfMatchingPairs(numbersZipped)
+}
+
+export const sumOfHalfWayWrappedPairs = (input) => {
+  const numbers = numberToArrayOfNumberParts(input)
+
+  const halfWayPoint = numbers.length / 2
+  const wrappedNumbers = _.concat(_.takeRight(numbers, halfWayPoint), _.take(numbers, halfWayPoint))
+  const numbersZipped = _.zip(numbers, wrappedNumbers)
+
+  return sumOfMatchingPairs(numbersZipped)
 }
--- src/day_01/index.test.js
+++ src/day_01/index.test.js
@@ -1,17 +1,41 @@
-import { sum } from './index'
+import { sumOfWrappedPairs, sumOfHalfWayWrappedPairs } from './index'
 
-test('1122 produces a sum of 3 (1 + 2)', () => {
-  expect(sum(1122)).toBe(3)
+describe('sumOfWrappedPairs', () => {
+  test('1122 produces a sum of 3 (1 + 2)', () => {
+    expect(sumOfWrappedPairs(1122)).toBe(3)
+  })
+
+  test('1111 produces 4 because each digit (all 1) matches the next', () => {
+    expect(sumOfWrappedPairs(1111)).toBe(4)
+  })
+
+  test('1234 produces 0 because no digit matches the next', () => {
+    expect(sumOfWrappedPairs(1234)).toBe(0)
+  })
+
+  test('91212129 produces 9', () => {
+    expect(sumOfWrappedPairs(91212129)).toBe(9)
+  })
 })
 
-test('1111 produces 4 because each digit (all 1) matches the next', () => {
-  expect(sum(1111)).toBe(4)
-})
+describe('sumOfHalfWayWrappedPairs', () => {
+  test('1212 produces 6', () => {
+    expect(sumOfHalfWayWrappedPairs(1212)).toBe(6)
+  })
 
-test('1234 produces 0 because no digit matches the next', () => {
-  expect(sum(1234)).toBe(0)
-})
+  test('1221 produces 0', () => {
+    expect(sumOfHalfWayWrappedPairs(1221)).toBe(0)
+  })
 
-test('91212129 produces 9', () => {
-  expect(sum(91212129)).toBe(9)
+  test('123425 produces 4', () => {
+    expect(sumOfHalfWayWrappedPairs(123425)).toBe(4)
+  })
+
+  test('123123 produces 12', () => {
+    expect(sumOfHalfWayWrappedPairs(123123)).toBe(12)
+  })
+
+  test('12131415 produces 4', () => {
+    expect(sumOfHalfWayWrappedPairs(12131415)).toBe(4)
+  })
 })
