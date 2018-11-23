2c2141c2786c69de0682a4eea8b9e471ef8a1459
Joe Wright <joe@joejag.com>
2018-11-23 01:14:44 +0000

    another impl - maybe not better

--- src/day_03/index.js
+++ src/day_03/index.js
@@ -7,28 +7,35 @@ export const manhattenDistanceToCenter = (cords) => {
 }
 
 export const spiralCordinatesFor = (number) => {
-  let spiralDepth = 0
-  let moves = []
-  const routeTaken = [{ number: 1, x: 0, y: 0 }]
-
-  while (_.last(routeTaken).number !== number) {
-    if (moves.length === 0) {
-      spiralDepth++
-      moves = _.flattenDeep([
-        _.times(1, _.constant((x, y) => ({ x: x + 1, y: y }))),
-        _.times((spiralDepth * 2) - 1, _.constant((x, y) => ({ x: x, y: y + 1 }))),
-        _.times(spiralDepth * 2, _.constant((x, y) => ({ x: x - 1, y: y }))),
-        _.times(spiralDepth * 2, _.constant((x, y) => ({ x: x, y: y - 1 }))),
-        _.times(spiralDepth * 2, _.constant((x, y) => ({ x: x + 1, y: y })))
-      ])
-    }
-
-    const previousPosition = _.last(routeTaken)
-    const newPosition = moves.shift()(previousPosition.x, previousPosition.y)
-    routeTaken.push({ number: previousPosition.number + 1, x: newPosition.x, y: newPosition.y })
+  let left = number - 1
+  let cords = { x: 0, y: 0 }
+  while (left > 0) {
+    cords = spiralStepper(cords)
+    left--
   }
+  return [cords.x, cords.y]
+}
 
-  // console.log('got here by', routeTaken)
+export const spiralStepper = ({ x, y }) => {
+  if (x === 0 && y === 0) return { x: 1, y: 0 }
 
-  return [_.last(routeTaken).x, _.last(routeTaken).y]
+  // top right, move LEFT now
+  if (Math.sign(y) === 1 && x === y) return { x: x - 1, y: y }
+  // top left, move DOWN now
+  if (Math.sign(y) === 1 && -x === y) return { x: x, y: y - 1 }
+  // bottom right: move RIGHT
+  if (Math.sign(y) === -1 && x === -y) return { x: x + 1, y: y }
+  // bottom left, move RIGHT now
+  if (Math.sign(y) === -1 && x === y) return { x: x + 1, y: y }
+
+  // on right: move UP
+  if (Math.sign(x) === 1 && Math.abs(x) > Math.abs(y)) return { x: x, y: y + 1 }
+  // on top: move LEFT
+  if (Math.sign(y) === 1 && Math.abs(y) > Math.abs(x)) return { x: x - 1, y: y }
+  // on left: move DOWN
+  if (Math.sign(x) === -1 && Math.abs(x) > Math.abs(y)) return { x: x, y: y - 1 }
+  // on bottom: move RIGHT
+  if (Math.sign(y) === -1 && Math.abs(y) > Math.abs(x)) return { x: x + 1, y: y }
+
+  return { error: 'what!' }
 }
--- src/day_03/index.test.js
+++ src/day_03/index.test.js
@@ -1,4 +1,5 @@
-import { manhattenDistanceToCenter, spiralCordinatesFor } from './index'
+import _ from 'lodash'
+import { manhattenDistanceToCenter, spiralCordinatesFor, spiralStepper } from './index'
 
 it('can work on manhatten distance', () => {
   expect(manhattenDistanceToCenter([0, 0])).toBe(0)
@@ -36,4 +37,89 @@ it('can compute the coordinate of given value in a spiral cube', () => {
   expect(spiralCordinatesFor(37)).toEqual([-3, 3])
   expect(spiralCordinatesFor(43)).toEqual([-3, -3])
   expect(spiralCordinatesFor(49)).toEqual([3, -3])
+})
+
+// Example spiral grid
+// 37  36  35  34  33  32  31
+// 38  17  16  15  14  13  30
+// 39  18   5   4   3  12  29
+// 40  19   6   1   2  11  28
+// 41  20   7   8   9  10  27
+// 42  21  22  23  24  25  26
+// 43  44  45  46  47  48  49  50
+it('knows to move right once at bottom right', () => {
+  expect(spiralStepper({ x: 0, y: 0 })).toEqual({ x: 1, y: 0 })
+  expect(spiralStepper({ x: 1, y: -1 })).toEqual({ x: 2, y: -1 })
+  expect(spiralStepper({ x: 2, y: -2 })).toEqual({ x: 3, y: -2 })
+})
+it('knows to move left once at the top right corner', () => {
+  expect(spiralStepper({ x: 1, y: 1 })).toEqual({ x: 0, y: 1 })
+  expect(spiralStepper({ x: 2, y: 2 })).toEqual({ x: 1, y: 2 })
+  expect(spiralStepper({ x: 3, y: 3 })).toEqual({ x: 2, y: 3 })
+})
+it('knows to move down once at the top left corner', () => {
+  expect(spiralStepper({ x: -1, y: 1 })).toEqual({ x: -1, y: 0 })
+  expect(spiralStepper({ x: -2, y: 2 })).toEqual({ x: -2, y: 1 })
+  expect(spiralStepper({ x: -3, y: 3 })).toEqual({ x: -3, y: 2 })
+})
+it('knows to move right once at the bottom left corner', () => {
+  expect(spiralStepper({ x: -1, y: -1 })).toEqual({ x: 0, y: -1 })
+  expect(spiralStepper({ x: -2, y: -2 })).toEqual({ x: -1, y: -2 })
+  expect(spiralStepper({ x: -3, y: -3 })).toEqual({ x: -2, y: -3 })
+})
+
+it('knows to move up if on the right hand side', () => {
+  expect(spiralStepper({ x: 1, y: 0 })).toEqual({ x: 1, y: 1 })
+
+  expect(spiralStepper({ x: 2, y: -1 })).toEqual({ x: 2, y: 0 })
+  expect(spiralStepper({ x: 2, y: 0 })).toEqual({ x: 2, y: 1 })
+  expect(spiralStepper({ x: 2, y: 1 })).toEqual({ x: 2, y: 2 })
+
+  expect(spiralStepper({ x: 3, y: -2 })).toEqual({ x: 3, y: -1 })
+  expect(spiralStepper({ x: 3, y: -1 })).toEqual({ x: 3, y: 0 })
+  expect(spiralStepper({ x: 3, y: 0 })).toEqual({ x: 3, y: 1 })
+  expect(spiralStepper({ x: 3, y: 1 })).toEqual({ x: 3, y: 2 })
+  expect(spiralStepper({ x: 3, y: 2 })).toEqual({ x: 3, y: 3 })
+})
+
+it('knows to move left if on the top side', () => {
+  expect(spiralStepper({ x: 0, y: 1 })).toEqual({ x: -1, y: 1 })
+
+  expect(spiralStepper({ x: 1, y: 2 })).toEqual({ x: 0, y: 2 })
+  expect(spiralStepper({ x: 0, y: 2 })).toEqual({ x: -1, y: 2 })
+  expect(spiralStepper({ x: -1, y: 2 })).toEqual({ x: -2, y: 2 })
+
+  expect(spiralStepper({ x: 2, y: 3 })).toEqual({ x: 1, y: 3 })
+  expect(spiralStepper({ x: 1, y: 3 })).toEqual({ x: 0, y: 3 })
+  expect(spiralStepper({ x: 0, y: 3 })).toEqual({ x: -1, y: 3 })
+  expect(spiralStepper({ x: -1, y: 3 })).toEqual({ x: -2, y: 3 })
+  expect(spiralStepper({ x: -2, y: 3 })).toEqual({ x: -3, y: 3 })
+})
+
+it('knows to move down if on the left hand side', () => {
+  expect(spiralStepper({ x: -1, y: 0 })).toEqual({ x: -1, y: -1 })
+
+  expect(spiralStepper({ x: -2, y: 1 })).toEqual({ x: -2, y: 0 })
+  expect(spiralStepper({ x: -2, y: 0 })).toEqual({ x: -2, y: -1 })
+  expect(spiralStepper({ x: -2, y: -1 })).toEqual({ x: -2, y: -2 })
+
+  expect(spiralStepper({ x: -3, y: 2 })).toEqual({ x: -3, y: 1 })
+  expect(spiralStepper({ x: -3, y: 1 })).toEqual({ x: -3, y: 0 })
+  expect(spiralStepper({ x: -3, y: 0 })).toEqual({ x: -3, y: -1 })
+  expect(spiralStepper({ x: -3, y: -1 })).toEqual({ x: -3, y: -2 })
+  expect(spiralStepper({ x: -3, y: -2 })).toEqual({ x: -3, y: -3 })
+})
+
+it('knows to move right if on the bottom side', () => {
+  expect(spiralStepper({ x: 0, y: -1 })).toEqual({ x: 1, y: -1 })
+
+  expect(spiralStepper({ x: -1, y: -2 })).toEqual({ x: 0, y: -2 })
+  expect(spiralStepper({ x: 0, y: -2 })).toEqual({ x: 1, y: -2 })
+  expect(spiralStepper({ x: 1, y: -2 })).toEqual({ x: 2, y: -2 })
+
+  expect(spiralStepper({ x: -2, y: -3 })).toEqual({ x: -1, y: -3 })
+  expect(spiralStepper({ x: -1, y: -3 })).toEqual({ x: 0, y: -3 })
+  expect(spiralStepper({ x: 0, y: -3 })).toEqual({ x: 1, y: -3 })
+  expect(spiralStepper({ x: 1, y: -3 })).toEqual({ x: 2, y: -3 })
+  expect(spiralStepper({ x: 2, y: -3 })).toEqual({ x: 3, y: -3 })
 })
